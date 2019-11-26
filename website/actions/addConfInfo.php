3bcf411adfce26aa46cca67cb3f6341fcea50176
Vitor Hugo <vhlg99@gmail.com>
2019-11-26 17:43:51 +0000

    added funcs

--- website/actions/addConfInfo.php
+++ website/actions/addConfInfo.php
@@ -2,13 +2,20 @@
 
 include_once('../database/user.php');
 
-$conf_name = $_POST['username'];
-$conf_code = $_POST['password'];
+$conf_name = $_POST['name'];
+$conf_code = $_POST['code'];
+$x_coords = $_POST['x'];
+$y_coords = $_POST['y'];
+$tags = $_POST['tag'];
+$first_ids = $_POST['first_id'];
+$second_ids = $_POST['second_id'];
 
 if($conf_name && $conf_code){
    if(validInfo($conf_name, $conf_code)){
        if(!confNameExists($conf_name)){
             addNewConf($conf_name, $conf_code);
+            addNodesToConf($conf_name,$x_coords, $y_coords, $tags);
+            addEdges();
         }
        else{
         $_SESSION["ERROR"] = "This conference already exists...";
--- website/database/confview.sql
+++ website/database/confview.sql
@@ -3,6 +3,7 @@
 DROP TABLE IF EXISTS conference;
 DROP TABLE IF EXISTS node;
 DROP TABLE IF EXISTS location;
+DROP TABLE IF EXISTS edge;
 
 
 CREATE TABLE conference (
@@ -11,15 +12,23 @@ CREATE TABLE conference (
 );
 
 CREATE TABLE node (
-    id                  INTEGER PRIMARY KEY,
+    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
     conference_name     INTEGER REFERENCES conference(confName) ON DELETE CASCADE,
     location_id         INTEGER REFERENCES location(id) ON DELETE CASCADE,
     isTag               BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE location (
-    id                   INTEGER PRIMARY KEY,
+    id                   INTEGER PRIMARY KEY AUTOINCREMENT,
     x_coord              REAL NOT NULL ON CONFLICT ABORT,
     y_coord              REAL NOT NULL ON CONFLICT ABORT
+);
+
+CREATE TABLE edge(
+
+    id                   INTEGER PRIMARY KEY AUTOINCREMENT,
+    origin_node          INTEGER REFERENCES node(id) ON DELETE CASCADE,        
+    dest_node            INTEGER REFERENCES node(id) ON DELETE CASCADE
+
 );
 
