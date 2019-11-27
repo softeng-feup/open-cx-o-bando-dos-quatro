f9b85402f5b248c814e56742c6b1b5e53d0178a3
antoniopedrodantas <pedro.dantascv@gmail.com>
2019-11-27 14:40:38 +0000

    database connection working

--- website/src/create.php
+++ website/src/create.php
@@ -1,13 +1,36 @@
 <?php
 
     // placeholder
-    $output = NULL;
+    //$output = NULL;
+
+    // include the database
+    include_once('../includes/include_data_base.php');
+
+    // access database
+    
+
 
     if(isset($_POST['submit'])){
 
         //when submitted
         $name = $_POST['name'];
         $code = $_POST['code'];
+
+        
+
+        
+
+        echo "Conference name: ";
+        echo $name;
+        echo "<br>";
+        
+        echo "Conference code: ";
+        echo $code;
+        echo "<br>";
+
+        $db = Database::instance()->db();
+
+        
 
         
 
@@ -19,13 +42,7 @@
         $second_id = $_POST['second_id'];
 
 
-        echo "Conference name: ";
-        echo $name;
-        echo "<br>";
         
-        echo "Conference code: ";
-        echo $code;
-        echo "<br>";
 
 
 
@@ -37,7 +54,7 @@
 
         for($i = 0; $i < $min; $i++) {
             echo $x[$keysOne[$i]] . "<br>";
-            echo $y[$keysTwo[$i]] . "<br><br>";
+            echo $y[$keysTwo[$i]] . "<br>";
             echo $tag[$keysThree[$i]] . "<br><br>";
         }
 
@@ -50,11 +67,18 @@
         for($j = 0; $j < $min_edge; $j++) {
             echo $first_id[$keysOne_edge[$j]] . "<br>";
             echo $second_id[$keysTwo_edge[$j]] . "<br><br>";
+
+            // code to input the edges into the database
+            // hardcoded for now, to test the for cycle
+
         }
 
 
        
-
+        // insert function here
+        // code to input into the databse hardcoded for now
+        $stmt = $db->prepare('INSERT INTO conference VALUES(?, ?)');
+        $stmt->execute(array($name, $code));
     }
 
 ?>
